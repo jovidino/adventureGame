902564323509aeadfaa0605b8ad2a25ce4458359
Austin Amon <austincamon@gmail.com>
2018-03-28 17:25:44 -0400

    Add files via upload

---
+++ Adventure_Game/program.cs
@@ -0,0 +1,115 @@
+using System;
+
+namespace Adventure_Game
+{
+    //Main Creature class with subclasses below it
+    class Creature
+    {
+        public string creatureName;
+        public int creatureHitPoints;
+
+        public Creature()
+        {
+
+        }
+        public Creature (string name, int hitPoints)
+        {
+            creatureName = name;
+            creatureHitPoints = hitPoints;
+        }
+
+
+    }
+    class Hero : Creature
+    {
+        string[] items;
+
+        public Hero () : base ()
+        {
+            
+        }
+        public Hero (string name, int hitPoints ) : base (name, hitPoints)
+        {
+            
+        }
+    }
+
+    class Goblin : Creature
+    {
+        public Goblin (string name, int hitPoints, int agro) : base(name, hitPoints)
+        {
+            name = "Goblin";
+            hitPoints = 10;
+            agro = 2;
+        }
+    }
+
+    class lesserDemon : Creature
+    {
+        public lesserDemon (string name, int hitPoints, int agro) : base(name, hitPoints)
+        {
+            name = "Lesser Demon";
+            hitPoints = 20;
+            agro = 4;
+        }
+    }
+    
+
+    //Main Weapons class with subclasses below it
+    class Weapons
+    {
+        private int physicalAttack;
+        private int magicAttack;
+        private int numSwings;
+
+        Weapon(int p, int m)
+        {
+            physicalAttack = p;
+            magicAttack = m;
+        }
+
+        public int getDamage(Creature c);
+    }
+
+    class Sword : Weapons
+    {
+        
+    }
+
+    class TwoHander : Weapons
+    {
+        //make sure when Two Hander is equiped have no other item in hand
+    }
+
+    //Armour class with subclasses below it
+    class Armor
+    {
+
+    }
+
+    
+    class Shield : Armor
+    {
+
+    }
+
+    class ChestPlate : Armor
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero you = new Hero();
+            //variables
+            string Cha
+            Console.WriteLine("Welcome to the Start of the Adventure Game");
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("What is your character's name? ");
+            CharacterName = Console.ReadLine();
+
+        }
+    }
+}
+
