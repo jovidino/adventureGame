95531f54783663982477c0d9cc5f53396702744f
jovidino <jovidino@yahoo.com>
2018-03-28 18:00:46 -0400

    test

--- Adventure_Game/Program.cs
+++ Adventure_Game/Program.cs
@@ -7,31 +7,69 @@ namespace Adventure_Game
     {
         public string creatureName;
         public int creatureHitPoints;
-
+        public bool isAlive;
+        public Weapons weapon;
+       
         public Creature()
         {
-
+            creatureName = "name";
+            creatureHitPoints = 0;
+            isAlive = true;
         }
         public Creature (string name, int hitPoints)
         {
             creatureName = name;
             creatureHitPoints = hitPoints;
+
+        }
+
+        public int getHP()
+        {
+            return this.creatureHitPoints;
+        }
+        
+        
+        //method for isAlive
+        public bool IsAlive()
+        {
+            if (this.creatureHitPoints > 0)
+                return true;
+            else
+                return false;
+        }
+
+
+        //method for fighting
+        public void fight(Creature c)
+        {
+            while(IsAlive() && c.IsAlive())
+            {
+                for(int i=0; i < weapon.numSwings; i++)
+                {
+
+                }
+            }
         }
 
 
     }
     class Hero : Creature
     {
-        string[] items;
+        public string[] items;
+        public string heroName;
 
         public Hero () : base ()
         {
-            
+            heroName = "";
+            creatureHitPoints = 50;
         }
         public Hero (string name, int hitPoints ) : base (name, hitPoints)
         {
-            
+            heroName = name;
+            hitPoints = 50;
         }
+
+       
     }
 
     class Goblin : Creature
@@ -58,6 +96,10 @@ namespace Adventure_Game
     //Main Weapons class with subclasses below it
     class Weapons
     {
+        public int numSwings;
+        public int magicAttack;
+        public int physicalAttack;
+
 
     }
 
@@ -87,18 +129,21 @@ namespace Adventure_Game
     {
 
     }
+
     class Program
     {
         static void Main(string[] args)
         {
             Hero you = new Hero();
             //variables
-            string Cha
+            string characterName;
             Console.WriteLine("Welcome to the Start of the Adventure Game");
             Console.WriteLine("-------------------------------------------");
             Console.WriteLine("What is your character's name? ");
-            CharacterName = Console.ReadLine();
-
+            characterName = Console.ReadLine();
+            you.heroName = characterName;
+            Console.WriteLine("Name is: " + you.heroName + " Hp is: " + you.getHP());
+            Console.ReadLine();
         }
     }
 }
